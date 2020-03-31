d38d6c4d61beda4c8bb8d31c7ad62aa4172a8493
Jonathan Ho <johnnyho82@gmail.com>
2020-03-31 18:41:23 -0400

    Update README and Favicon

--- open-brewery/src/components/Header.js
+++ open-brewery/src/components/Header.js
@@ -1,71 +1,70 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-
 import { useDarkMode } from '../hooks/useDarkMode';
-
 import { states } from '../states';
-
 import { getData } from '../store/actions';
 
 export default function Header() {
-  const [search, setSearch] = useState('');
-  const dispatch = useDispatch();
-  const [darkMode, setDarkMode] = useDarkMode(false);
+	const [search, setSearch] = useState('');
 
-  const toggleMode = e => {
-    e.preventDefault();
-    setDarkMode(!darkMode);
-  };
+	const dispatch = useDispatch();
 
-  const handleSelect = e => {
-    dispatch(getData(`?by_state=${e.target.value}&per_page=50`));
-    setSearch('');
-  };
+	const [darkMode, setDarkMode] = useDarkMode(false);
 
-  const handleClick = e => {
-    e.preventDefault();
-    dispatch(getData(`/search?query=${search}&per_page=50`));
-  };
+	const toggleMode = e => {
+		e.preventDefault();
+		setDarkMode(!darkMode);
+	};
 
-  const handleKeyDown = e => {
-    if (e.key === 'Enter') {
-      e.preventDefault();
-      dispatch(getData(`/search?query=${search}&per_page=50`));
-    }
-  };
+	const handleSelect = e => {
+		dispatch(getData(`?by_state=${e.target.value}&per_page=50`));
+		setSearch('');
+	};
 
-  return (
-    <div className='header'>
-      <h1>
-        <span role='img' aria-label='beers emoji'>
-          🍻
-        </span>{' '}
-        Find a Brewery!{' '}
-        <span role='img' aria-label='beers emoji'>
-          🍻
-        </span>
-      </h1>
-      <select onChange={handleSelect}>
-        <option>Select a state...</option>
-        {states.map(state => (
-          <option
-            key={state.abbreviation}
-            value={state.name.split(' ').join('_')}>
-            {state.name}
-          </option>
-        ))}
-      </select>
-      <div className='search'>
-        <input
-          type='text'
-          placeholder='...search'
-          value={search}
-          onChange={e => setSearch(e.target.value)}
-          onKeyDown={e => handleKeyDown(e)}
-        />
-        <button onClick={handleClick}>Go!</button>
-      </div>
-      <div onClick={toggleMode} className='toggle'></div>
-    </div>
-  );
+	const handleClick = e => {
+		e.preventDefault();
+		dispatch(getData(`/search?query=${search}&per_page=50`));
+	};
+
+	const handleKeyDown = e => {
+		if (e.key === 'Enter') {
+			e.preventDefault();
+			dispatch(getData(`/search?query=${search}&per_page=50`));
+		}
+	};
+
+	return (
+		<div className='header'>
+			<h1>
+				<span role='img' aria-label='beers emoji'>
+					🍻
+				</span>{' '}
+				Find a Brewery!{' '}
+				<span role='img' aria-label='beers emoji'>
+					🍻
+				</span>
+			</h1>
+			<select onChange={handleSelect}>
+				<option>Select a state...</option>
+				{states.map(state => (
+					<option
+						key={state.abbreviation}
+						value={state.name.split(' ').join('_')}>
+						{state.name}
+					</option>
+				))}
+			</select>
+			<div className='search'>
+				<input
+					type='text'
+					placeholder='...search'
+					value={search}
+					onChange={e => setSearch(e.target.value)}
+					onKeyDown={e => handleKeyDown(e)}
+				/>
+				<button onClick={handleClick}>Go!</button>
+			</div>
+			<div onClick={toggleMode} className='toggle'></div>
+		</div>
+	);
 }
